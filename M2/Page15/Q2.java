ec3f50aa7f4aef040064cbd2a972f52fcd386528
gauravsdevelop <gauravsdevelop@gmail.com>
2024-09-30 23:04:52 +0530

    added m2 page 20,19

--- M2/Page15/Q2.java
+++ M2/Page15/Q2.java
@@ -1,4 +1,38 @@
-import java.util.*;
+// Valid Parentheses 4
+
+
+
+// import java.util.*;
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Input Format
+
+// 1 <= s.length <= 10^4
+// s consists of parentheses only '()[]{}'
+// Constraints
+
+// 1 <= s.length <= 10^4
+
+// Output Format
+
+// print 'true' or 'false'
+
+// Sample Input 0
+
+// ()[]{}
+// Sample Output 0
+
+// true
+// Sample Input 1
+
+// (]
+// Sample Output 1
+
+// false
 
 public class Q2 {
 
