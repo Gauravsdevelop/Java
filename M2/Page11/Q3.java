7e01267d4963130aa85ef24aad505b82eeb4dfd4
gauravsdevelop <gauravsdevelop@gmail.com>
2024-10-01 02:01:21 +0530

    m2 updated

---
+++ M2/Page11/Q3.java
@@ -0,0 +1,27 @@
+// HW_palindromic string 13
+
+
+// A palindrome is a term that can be read the same way forwards and backwards, irrespective of any punctuation or capitalization changes. To determine whether a phrase is a palindrome, the phrase is first converted to lowercase, and all non-alphanumeric characters are removed, leaving only letters and numbers.
+
+// Input Format
+
+// string str as an input.
+
+// Constraints
+
+// 1 <= str.length <= 2 * 10^5
+
+// str consists only of printable ASCII characters.
+
+// Output Format
+
+// return true or false.
+
+// Sample Input 0
+
+// A man, a plan, a canal: Panama
+// Sample Output 0
+
+// true
+
+
---
+++ M2/Page11/Q9.java
@@ -0,0 +1,26 @@
+// HW_Longest Palindromic Substring 13
+
+// Given a string str, return the longest palindromic substring in str.
+
+// NOTE :-
+
+// After answering the question, attempt the related question in the linked resource to improve your understanding of the question . Clickhere
+
+// Input Format
+
+// string str as an input.
+
+// Constraints
+
+// 1 <= str.length <= 10^2
+
+// Output Format
+
+// return the longest palindromic substring.
+
+// Sample Input 0
+
+// cbbd
+// Sample Output 0
+
+// bb
